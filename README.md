95d30e0266ad15135ebd5474fbd06aea83aa4294
yousefelsemeen <yousefelsemeen@gmail.com>
2022-08-15 19:46:22 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Spell Checker:
+>### This is a small project <span style="color: navy;">*- we had in college (level 2) -*</span> in python that takes a sentence from the user and prints the same sentence with misspelled words underlined and top suggestions for those misspelled words if there are some in next lines.
+# Example:
+<img src="imgs/1.png" width="100%"></img>
+<img src="imgs/2.png" width="100%"></img>
---
+++ main.py
@@ -0,0 +1,20 @@
+from methods import *
+from time import time # To calculate execution time for algorithms
+
+# Those digits and symbols are for colorizing text in console
+sentence = input("\033[38;5;231mType words to check its spelling:\033[0m\n")
+words_list = sentence.lower().split()
+
+start = time()
+
+dictionary = get_dictionary()
+
+mispelled_words = get_misspelled(dictionary, words_list)
+
+print_underlined(words_list, mispelled_words)
+
+suggestions = get_suggestions(dictionary, mispelled_words)
+
+print_suggestions(mispelled_words, suggestions)
+
+print(f"\033[38;5;231m\nProgram executed in: {time() - start} sec\033[0m")
